f2ff1add3eedb0f892fa4b716e5132988fed1d4a
TUB-Hasib <h.alam@tu-berlin.de>
2022-10-25 02:50:25 +0200

    task1

---
+++ task1.py
@@ -0,0 +1,52 @@
+
+def is_even(number):
+    """input: int number
+    output: bool. true for even. false for odd num"""
+    if number % 2 == 0:
+        return True
+    else:
+        return False
+
+"""
+def find_steps_and_path_collatz_conjecture(number):
+    if number <= 0:
+        print("Error, Given No have to be a positive non zero Number.")
+        return False
+    if number == 1:
+        return [1, [1]]
+
+    total_steps = 0
+    paths = []
+    while number > 1:
+        if is_even(number):
+            number = int(number / 2)
+        else:
+            number = 3 * number + 1
+
+        paths.append(number)
+        total_steps = total_steps + 1
+    return [total_steps, paths]
+"""
+
+def find_steps_of_collatz_conjecture(number):
+    """input: int number
+        output: int. no of steps need to reach 1 for the given number"""
+    if number <= 0:
+        print("Error, Given No have to be a positive non zero Number.")
+        return False
+    if number == 1:
+        return 0
+
+    total_steps = 0
+    while number > 1:
+        if is_even(number):
+            number = int(number / 2)
+        else:
+            number = 3 * number + 1
+
+        total_steps = total_steps + 1
+    return total_steps
+
+
+if __name__ == '__main__':
+    print(find_steps_of_collatz_conjecture(3))
